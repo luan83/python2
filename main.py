3eaf103e7fbf91d50a3d40e40e902da0a443399f
luan83 <68071485+luan83@users.noreply.github.com>
2024-02-29 08:41:53 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,41 +1,23 @@
-import random
+palavra_secreta = "sanduba"
+letras_acertadas = ["_","_","_","_","_","_","_"]
+tentativas = 10
 
-def jogar_adivinhar():
-    
-    nivel = int(input("escolha a dificuldadde(1-facil, 2-medio, 3-dificil):"))
-    max_numero = 10 if nivel == 1 else 50 if nivel == 2 else 100
-    numero_secreto = random.randint(1, max_numero)
-    tentativas = 10 if nivel == 1 else 5 if nivel == 2 else 3 
-    pontos = 1000
-    
-    print(f"jogo de adivinhacao - nivel {nivel}")
-    print(f"tente adivinhar o numero que estou pensando, entre 1 e {max_numero}.")
-    
-    for tentativas in range(1, tentativas + 1):
-        print(f"tentativa {tentativa} de {tentativas}")
-        palpite = int(input("digite seu palpite"))
+while tentativas > 0 and "_" in letras_acertadas:
+    palpite = input("Digite uma letra: ").lower()
+
+    if palpite in palavra_secreta:
+        index = 0
+    for letra in palavra_secreta:
+        if palpite == letra:
+            letras_acertadas[index] = letra
+        index += 1
+    else: 
+        tentativas -= 1
+        print(f"voce tem {tentativas} tentativas restantes.")
         
-        if palpite < 1 or palpite > max_numero:
-            pint(f"digite um numero entre 1 e {max_numero}.")
-            continue
-        acertou = palpite == numero_secreto
-        maior = palpite > numero_secreto
-        menor = palpite < numero_secreto
+    print(" ".join(letras_acertadas))
         
-        if acertou:
-            print(f"voce acertou e fez {pontos}pontos!")
-            break
-        else:
-            pontos_perdidos = abs(numero_secreto - palpite)
-            pontos -= pontos_perdidos
-            if maior:
-                print("seu palpite foi maior do que o numero secreto.")
-            elif menor:
-                    print("seu palpite foi menor do que o numero secreto.")
-                    
-        if not acertou:
-            print(f"suas tentativas acabaram. o numero era {numero_secreto}.")
-        print("fim de jogo!")
-        
-if__name__ =="__name__"
-    jogar_adivinhar()
+if "_" not in letras_acertadas:
+    print("parabens TU GANHOUUUU INUTIL!")
+else: 
+    print("que pena voce perdeu playboy, a palavra era sanduba rapa.")
